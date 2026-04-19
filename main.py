a265171f86ea53052af0e4a84c52d7c41715080a
liam274 <ezyjsdeveloper@gmail.com>
2026-04-19 15:45:33 +0800

    start

---
+++ main.py
@@ -0,0 +1,13 @@
+"""
+file: main.py
+author: liam274
+
+This is a project which helps people reduce the self-studying cost, aiming to improve the accurency.
+
+"""
+import os
+
+class parser:
+    def __init__(path: str):
+        this.path=path
+        
